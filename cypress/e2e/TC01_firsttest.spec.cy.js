3f7a3298fc18d22ab9d2d64a8c64ce3c1dce6536
apatel211 <ankita.it.2012@gmail.com>
2024-01-23 13:42:43 -0500

    Initial commit two

--- cypress/e2e/TC01_firsttest.spec.cy.js
+++ cypress/e2e/TC01_firsttest.spec.cy.js
@@ -6,8 +6,8 @@ describe('Login Functionality',function(){
     it('login test', function(){
         cy.visit('/')
         cy.get('a[href="#login"]').click()
-        cy.get('input[placeholder="Email"]').type('ankita.it.2012@gmail.com')
-        cy.get('input[placeholder="Password"]').type('@Test12345')
+        cy.get('input[placeholder="Email"]').type('fakepaxankita12@gmail.com')
+        cy.get('input[placeholder="Password"]').type('Abcd@12345')
         cy.get('button[type="Submit"]').click()
         cy.wait(10000);
         cy.get('a[href="#settings"]').click()
